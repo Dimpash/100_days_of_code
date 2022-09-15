e2d377f0ae33ae1ddd99b336b971801b77ea121b
Dimpash <100344777+Dimpash@users.noreply.github.com>
2022-09-15 17:02:39 +0300

    Add files via upload

---
+++ Email/main.py
@@ -0,0 +1,44 @@
+##################### Extra Hard Starting Project ######################
+
+# 1. Update the birthdays.csv
+
+# 2. Check if today matches a birthday in the birthdays.csv
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+# 4. Send the letter generated in step 3 to that person's email address.
+
+
+import pandas
+import datetime as dt
+import smtplib
+from random import randint
+
+MY_EMAIL = "main-it@esbit.vitebsk.energo.net"
+MY_PASSWORD = "20011954"
+
+# Angela version
+#Bad thing: it works only for one person in file
+# today = (dt.datetime.now().month, dt.datetime.now().day)
+# data = pandas.read_csv("birthdays.csv")
+# birthdays_dict = {(row["month"], row["day"]): row for index, row in data.iterrows()}
+
+
+# My version
+birthdays = pandas.read_csv("birthdays.csv")
+now_month = dt.datetime.now().month
+now_day = dt.datetime.now().day
+
+for index, row in birthdays.iterrows():
+    if row.day == now_day and row.month == now_month:
+        file_name = f"letter_templates\letter_{randint(1,3)}.txt"
+        with open(file_name, "r") as file:
+            message = f"Subject:Happy birthday to you!\n\n{file.read().replace('[NAME]', row['name'])}"
+        with smtplib.SMTP("mail.esbit.vitebsk.energo.net") as connection:
+            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+            connection.sendmail(from_addr=MY_EMAIL, to_addrs=row["email"], msg=message)
+
+
+
+
+
---
+++ Email/sending_emails.py
@@ -0,0 +1,26 @@
+import smtplib
+import datetime as dt
+import random
+
+
+# my_email = "mitya.p.76.exp@gmail.com"
+# password = "exp,123698745"
+# destination_email = "mitya.p.76@gmail.com"
+MY_EMAIL = "main-it@esbit.vitebsk.energo.net"
+MY_PASSWORD = "20011954"
+destination_email = "mitya_76@mail.ru"
+
+now = dt.datetime.now()
+now_weekday = now.weekday()
+
+with open("quotes.txt", "r") as f:
+    quotes = f.readlines()
+
+if now_weekday == 3:
+    quote = random.choice(quotes)
+    message = f"Subject:Motivation quote\n\n{quote}"
+    with smtplib.SMTP("mail.esbit.vitebsk.energo.net") as connection:
+        # connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs=destination_email, msg=message)
+
